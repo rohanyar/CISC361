ecd56a4248a88d3f4b64bc02be8616f2a18719f4
rohanyar <99371548+rohanyar@users.noreply.github.com>
2022-09-11 14:50:50 -0400

    Update prototypes.h

--- Yarlagadda_proj_1a/prototypes.h
+++ Yarlagadda_proj_1a/prototypes.h
@@ -1,1 +1,21 @@
+typedef struct student {
+  char* lastname;
+  char* firstname;
+  long id;
+  char* year;
+  int graduation;
+  Student* next;
+  Student* prev;
+} Student;
 
+
+
+
+
+The last name of the student stored in a char*
+The first name of the student stored in a char*
+The student's id number stored as a long
+The year of the student (i.e. freshman, sophomore) stored in a char*
+The year of expected graduation stored as an int
+A "next" pointer to another one of these structs (so you can make a linked list)
+A "prev" pointer to another one of these structs (so you can make it a doubly linked list)
