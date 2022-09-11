e063b5abeb0fd3bb61cda945d4f1c88b617472df
rohanyar <rohan3yarlagadda@gmail.com>
2022-09-11 15:21:04 -0400

    commented requirements

--- Yarlagadda_proj_1a/prototypes.h
+++ Yarlagadda_proj_1a/prototypes.h
@@ -12,10 +12,10 @@ typedef struct student {
 
 
 
-The last name of the student stored in a char*
-The first name of the student stored in a char*
-The student's id number stored as a long
-The year of the student (i.e. freshman, sophomore) stored in a char*
-The year of expected graduation stored as an int
-A "next" pointer to another one of these structs (so you can make a linked list)
-A "prev" pointer to another one of these structs (so you can make it a doubly linked list)
+//The last name of the student stored in a char*
+//The first name of the student stored in a char*
+//The student's id number stored as a long
+//The year of the student (i.e. freshman, sophomore) stored in a char*
+//The year of expected graduation stored as an int
+//A "next" pointer to another one of these structs (so you can make a linked list)
+//A "prev" pointer to another one of these structs (so you can make it a doubly linked list)
