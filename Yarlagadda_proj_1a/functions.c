604aabb9207345a78acb587526f829a1295eb64b
rohanyar <rohan3yarlagadda@gmail.com>
2022-09-14 23:00:24 -0400

    added to add function

--- Yarlagadda_proj_1a/functions.c
+++ Yarlagadda_proj_1a/functions.c
@@ -2,26 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include "prototypes.h"
+#define MAX 128
 
 int main() {
-    sList slist = (sList)malloc(sizeof(sList));
+    sList slist = (sList*)malloc(sizeof(sList));
 }
 
 
 
 void addStudent(sList slist) {
+    char str[MAX];
+    int len;
     Student *student = (Student*)malloc(sizeof(Student));
     
     printf("Student last name: ");
-    scanf("%s", student->lastname);
+    if (fgets(str, MAX , stdin) != NULL) {
+        len = (int)strlen(str);
+        str[len-1] = '\0';
+        student->lastname = (char*)malloc(len);
+        strcopy(student->lastname, str);
+    }
     printf("Student first name: ");
-    scanf("%s", student->firstname);
+    if (fgets(str, MAX , stdin) != NULL) {
+        len = (int)strlen(str);
+        str[len-1] = '\0';
+        student->firstname = (char*)malloc(len);
+        strcopy(student->firstname, str);
+    }
     printf("Student id: ");
-    scanf("%ld", &student->id);
+    if (fgets(str, MAX , stdin) != NULL) {
+        len = (int)strlen(str);
+        str[len-1] = '\0';
+        student->id = (char*)malloc(len);
+        strcopy(student->id, str);
+    }
     printf("Student year: ");
-    scanf("%s", student->year);
+    if (fgets(str, MAX , stdin) != NULL) {
+        len = (int)strlen(str);
+        str[len-1] = '\0';
+        student->year = (char*)malloc(len);
+        strcopy(student->year, str);
+    }
     printf("Student graduation: ");
-    scanf("%d", &student->graduation);
+    if (fgets(str, MAX , stdin) != NULL) {
+        len = (int)strlen(str);
+        str[len-1] = '\0';
+        student->graduation = (char*)malloc(len);
+        strcopy(student->graduation, str);
+    }
     if(slist->head)
 }
 
