a73cce1ab98e9ab8f0c71807a9d18f6ba5f2a59d
rohanyar <rohan3yarlagadda@gmail.com>
2022-09-17 00:43:16 -0400

    add

--- Yarlagadda_proj_1a/functions.c
+++ Yarlagadda_proj_1a/functions.c
@@ -13,6 +13,7 @@ int main()
     char lastname[MAX];
     char firstname[MAX];
     char year[MAX];
+    int len;
     printf("\n 1: Add Student\n 2: Delete Student\n 3: Print Beginning to End\n 4: Print End to Beginning\n 5: Exit\n");
     int decision = 0;
     if (fgets(str, MAX, stdin) != NULL)
@@ -26,14 +27,21 @@ int main()
         {
             Student *student = (Student *)malloc(sizeof(Student));
             printf("Student last name: ");
-            if (fgets(lastname, MAX, stdin) != NULL)
+            if (fgets(lastname, MAX, stdin) != NULL) 
             {
-                lastname[strlen(lastname) - 1] = '\0';
+                len = (int)strlen(lastname);
+                lastname[len - 1] = '\0';
+                student->lastname = (char*)(malloc(len));
+                strcpy(student->lastname, lastname);
             }
+    
             printf("Student first name: ");
             if (fgets(firstname, MAX, stdin) != NULL)
             {
-                firstname[strlen(firstname) - 1] = '\0';
+                len = (int)strlen(firstname);
+                firstname[len - 1] = '\0';
+                student->firstname = (char*)(malloc(len));
+                strcpy(student->firstname, firstname);
             }
             printf("Student id: ");
             if (fgets(str, MAX, stdin) != NULL)
@@ -43,14 +51,17 @@ int main()
             printf("Student year: ");
             if (fgets(year, MAX, stdin) != NULL)
             {
-                year[strlen(year) - 1] = '\0';
+                len = (int)strlen(year);
+                year[len - 1] = '\0';
+                student->year = (char*)(malloc(len));
+                strcpy(student->year, year);
             }
             printf("Student graduation: ");
             if (fgets(str, MAX, stdin) != NULL)
             {
                 student->graduation = atoi(str);
             }
-            addStudent(lastname, firstname, student->id, year, student->graduation);
+            addStudent(student->lastname, student->firstname, student->id, student->year, student->graduation);
             printf(" 1: Add Student\n 2: Delete Student\n 3: Print Beginning to End\n 4: Print End to Beginning\n 5: Exit\n");
             if (fgets(str, MAX, stdin) != NULL)
             {
@@ -68,12 +79,16 @@ int main()
         }
         else if (decision == 2)
         {
-            printf("Enter last name of student to delete: \n");
-            if (fgets(str, MAX, stdin) != NULL)
+            printf("Enter last name of student to delete:\n");
+            Student *student = (Student *)malloc(sizeof(Student));
+            if (fgets(lastname, MAX, stdin) != NULL)
             {
-                str[strlen(str) - 1] = '\0';
+                len = (int)strlen(lastname);
+                lastname[len - 1] = '\0';
+                student->lastname = (char*)(malloc(len));
+                strcpy(student->lastname, lastname);
             }
-            deleteStudent(str);
+            deleteStudent(student->lastname);
             printf(" 1: Add Student\n 2: Delete Student\n 3: Print Beginning to End\n 4: Print End to Beginning\n 5: Exit\n");
             if (fgets(str, MAX, stdin) != NULL)
             {
